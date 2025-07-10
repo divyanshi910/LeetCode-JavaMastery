3b0e5184c1f39e067e80327cf94976c0a1b2dd09
Divyanshi Verma <divyanshiverma910@gmail.com>
2025-07-10 15:31:17 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (84.00%) - LeetSync

---
+++ 733-flood-fill/flood-fill.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int currentColor=image[sr][sc];
+        if(currentColor == color){
+            return image;
+        }
+        //color different
+        
+            dfs(image,sr,sc,color,currentColor);
+            return image;
+        
+    }
+
+    void dfs(int[][] image, int sr, int sc, int newColor, int orgColor){
+        //base case
+        if(sr<0 || sr>=image.length || sc<0 ||sc>=image[0].length ||image[sr][sc]!=orgColor){
+            return ;//backtrack
+        }
+        image[sr][sc] = newColor;//mark as visited
+        dfs(image, sr+1 , sc, newColor ,orgColor);//Move Down
+        dfs(image, sr-1 , sc, newColor ,orgColor);//Move Up
+        dfs(image, sr , sc+1, newColor ,orgColor);//Move Right
+        dfs(image, sr, sc-1, newColor ,orgColor);//Move Down
+    }    
+    }
