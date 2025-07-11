ebad30d731db912912da08d27cf14c1506f4c360
Divyanshi Verma <divyanshiverma910@gmail.com>
2025-07-11 14:12:47 +0530

    Time: 2 ms (96.77%) | Memory: 41.6 MB (81.86%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean compareBracket(char opening , char closing){
+        return ((opening=='(' && closing ==')') || (opening=='[' && closing ==']') || (opening=='{' && closing =='}') );
+    }
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i =0 ;i<s.length();i++){
+            char bracket = s.charAt(i);
+            if(bracket=='(' || bracket=='{' || bracket=='['){
+                stack.push(bracket);
+            }
+            else if(!stack.isEmpty() && compareBracket(stack.peek(),bracket)){
+                stack.pop();
+            }
+            else{
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
