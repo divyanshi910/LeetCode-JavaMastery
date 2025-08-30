7850fe0b328537f6eea59882ce66271126cb30c5
Divyanshi Verma <divyanshiverma910@gmail.com>
2025-08-30 19:26:55 +0530

    Time: 3 ms (39.73%) | Memory: 42.2 MB (13.47%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -4,8 +4,8 @@ class Solution {
     }
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-        for(int i =0 ;i<s.length();i++){
-            char bracket = s.charAt(i);
+           for(int i =0 ;i<s.length();i++){ 
+           char bracket = s.charAt(i);
             if(bracket=='(' || bracket=='{' || bracket=='['){
                 stack.push(bracket);
             }
